a7eba1ad9770c6d5ff71801a3a3f4ca333bee4ff
Boston <bak45247@bethel.edu>
2020-01-25 11:39:03 -0600

    polygon class and implementations

--- Lectures/Lecture1/src/Polygon.java
+++ Lectures/Lecture1/src/Polygon.java
@@ -3,5 +3,21 @@ public class Polygon {
     private int numVerticies;
     private Point verticies[];
 
-    
+    // look at lecture code to fill this class
+
+    // Example moving into movie distributor class
+    // returns the max X coordinate in the list of verticies
+    // If it was me, I would return the point its at but...
+    public double maxXCoodinate(){
+        // double output = 0; I hate this
+
+        double maxX = this.verticies[0].getX();
+
+        for(int i = 0; i < this.numVerticies; i++){
+            if(this.verticies[i].getX() > maxX);
+                maxX = this.verticies[i].getX();
+        }
+
+        return maxX;
+    }
 }
